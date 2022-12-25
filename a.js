17f94e46591081d9ec030b5083da0e5242bcf584
tanjinaesha <eshaislam15@gmail.com>
2022-12-25 14:59:15 +0600

    js_10

---
+++ a.js
@@ -0,0 +1,7 @@
+var num = prompt("Please enter your age");
+		if(num % 5 == 0 && num % 11 == 0)
+		console.log("Number is divisible by 5 and 11");
+		else
+    {
+        console.log("Number is not divisible by 5 and 11");
+    }
